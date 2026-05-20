60c1c717121ccc0559f2c3aad0edf09c7d9737c8
Link4Electronics <link4electronics@gmail.com>
2026-05-19 22:35:04 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/arx-libertatis.png
 export DESKTOP=/usr/share/applications/arx-libertatis.desktop
-export STARTUPWMCLASS=
+export STARTUPWMCLASS=arx-libertatis
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
