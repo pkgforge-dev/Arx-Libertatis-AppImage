7cd4b2a71a9a0a0416bd4b3c70de2a345b65c1fc
Link4Electronics <link4electronics@gmail.com>
2026-09-09 03:30:40 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,4 +28,18 @@ get-debloated-pkgs --add-common --prefer-nano
 #fi
 #make-aur-package "$package"
 #pacman -Q "$package" | awk '{print $2; exit}' > ~/version
+echo "Building Arx Libertatis..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/arx/ArxLibertatis"
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+    echo "Making nightly build of Arx Libertatis..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone --depth 1 "$REPO" ./ArxLibertatis
+else
+	echo "Making stable build of Arx Libertatis..."
+	VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
+	git clone --branch v"$VERSION" --single-branch --depth 1 "$REPO" ./ArxLibertatis
+fi
+echo "$VERSION" > ~/version
 
